1740786317fed046ffd27136de38d675230396f6
Mohammed Shaik Sahil <131136485+sahisani1305@users.noreply.github.com>
2025-04-18 14:04:29 +0530

    Update static file settings and template references for improved management

--- api/settings.py
+++ api/settings.py
@@ -119,5 +119,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# settings.py
+import os
+
+# Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Additional locations of static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
