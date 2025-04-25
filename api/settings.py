3c663a310f1e8231299300e088e3287d5311d0c1
Mohammed Shaik Sahil <131136485+sahisani1305@users.noreply.github.com>
2025-04-25 20:49:05 +0530

    Refactor code structure for improved readability and maintainability

--- api/settings.py
+++ api/settings.py
@@ -113,7 +113,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+import os
+
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # Set a path for collected static files
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
